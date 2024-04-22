2afdc9e67ac54b68eef9af40c47ce865ac1d2d8e
LiangHeng <lianghengcn@gmail.com>
2024-04-22 18:26:23 +0800

    feat: add tencent cos and sts service api feat: add dotenv dependency to read .env file

---
+++ API/ImageBucket/index.ts
@@ -0,0 +1,92 @@
+import COS from "cos-nodejs-sdk-v5"
+import STS from 'qcloud-cos-sts'
+export class ImageBucket {
+    private static Config = {
+        baseKey: '/chat/image/',
+        Bucket: 'imagebucket-1322308688',
+        Region: 'ap-tokyo'
+    }
+    private secretId: string;
+    private secretKey: string;
+    private cos: COS;
+    private sts: tempCredentialGenerator;
+    constructor(SecretKey: string, SecretId: string) {
+        this.secretId = SecretId;
+        this.secretKey = SecretKey;
+        this.cos = new COS({
+            SecretKey, SecretId
+        });
+        this.sts = new tempCredentialGenerator(SecretKey, SecretKey);
+    };
+    public async uploadFile(data: Buffer | string, key: string) {
+        try {
+            const response = this.cos.putObject({
+                Bucket: ImageBucket.Config.Bucket,
+                Region: ImageBucket.Config.Region,
+                StorageClass: 'STANDARD',
+                Key: ImageBucket.Config.baseKey + key,
+                Body: data
+            })
+            return (await response).Location;
+        } catch (error) {
+            throw new Error('fail at uploadFile');
+        }
+    }
+}
+
+export class tempCredentialGenerator {
+    private config: {
+        secretId: string,
+        secretKey: string,
+        durationSeconds: number,
+        bucket: string,
+        region: string,
+        allowPrefix: string,
+        proxy: string,
+    };
+    private static policy = {
+        'version': '2.0',
+        'statement': [{
+            'action': [
+                // 简单上传
+                'name/cos:PutObject',
+                'name/cos:PostObject',
+                // 分片上传
+                'name/cos:InitiateMultipartUpload',
+                'name/cos:ListMultipartUploads',
+                'name/cos:ListParts',
+                'name/cos:UploadPart',
+                'name/cos:CompleteMultipartUpload',
+                // 简单上传和分片，需要以上权限，其他权限列表请看 https://cloud.tencent.com/document/product/436/31923
+
+                // 文本审核任务
+                // 开通媒体处理服务
+                // 更多数据万象授权可参考：https://cloud.tencent.com/document/product/460/41741
+            ],
+            'effect': 'allow',
+            'principal': { 'qcs': ['*'] },
+        }],
+    }
+    constructor(SecretKey: string, SecretId: string) {
+        this.config = {
+            secretId: SecretId,
+            secretKey: SecretKey,
+            durationSeconds: 1800,
+            bucket: 'imagebucket-1322308688',
+            region: 'ap-tokyo',
+            allowPrefix: '/chat/image/*',
+            proxy: ''
+        }
+    }
+    public async getCredential() {
+        try {
+            const credential = STS.getCredential({
+                ...this.config,
+                policy: tempCredentialGenerator.policy
+            })
+            return credential;
+        } catch {
+            throw new Error('fail at get temp credential.');
+        }
+    }
+}
--- index.ts
+++ index.ts
@@ -5,14 +5,12 @@ import { router } from './router';
 import { createSocket } from "./Socket.io/socket";
 import { createServer } from "http";
 import { env } from "process";
+import 'dotenv/config';
 const app = express();
 const httpServer = createServer(app);
 // if (process.env.MONGODB_URI) {
 connectToMongo(process.env.MONGODB_ADMIN + '/chat');
 console.log(process.env.MONGODB_ADMIN + '/chat');
-// } else {
-//     console.error('moongodb uri env variable not found');
-// }
 const io = createSocket(httpServer);
 app.use(bodyParser.json());
 app.use('/', router);
